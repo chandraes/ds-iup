415147c2b4c2afae105f9beeaa25f3d3abf15af2
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-07-15 15:41:02 +0700

    Update column label in barang index view

--- resources/views/db/stok-non-ppn/index.blade.php
+++ resources/views/db/stok-non-ppn/index.blade.php
@@ -87,7 +87,7 @@
                     <th class="text-center align-middle" style="width: 15px">No</th>
                     <th class="text-center align-middle">Unit</th>
                     <th class="text-center align-middle">Tipe</th>
-                    <th class="text-center align-middle">Kategori</th>
+                    <th class="text-center align-middle">Kelompok<br>Barang</th>
                     <th class="text-center align-middle">Nama<br>Barang</th>
                     <th class="text-center align-middle">Kode<br>Barang</th>
                     <th class="text-center align-middle">Merk<br>Barang</th>
