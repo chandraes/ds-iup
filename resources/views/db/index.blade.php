c4e20e22a57c6328ada389292598a1c5871f7d43
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-07-15 15:22:40 +0700

    Update index.blade.php to add links for stok-ppn and stok-non-ppn pages

--- resources/views/db/index.blade.php
+++ resources/views/db/index.blade.php
@@ -42,40 +42,6 @@
     </div>
     <div class="row justify-content-left">
         <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
-            <a href="{{route('db.staff')}}" class="text-decoration-none">
-                <img src="{{asset('images/karyawan.svg')}}" alt="" width="70">
-                <h5 class="mt-2">BIODATA<br>DIREKSI & STAFF</h5>
-            </a>
-        </div>
-        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
-            <a href="{{route('db.supplier')}}" class="text-decoration-none">
-                <img src="{{asset('images/supplier.svg')}}" alt="" width="70">
-                <h5 class="mt-2">BIODATA<br>SUPPLIER<br></h5>
-            </a>
-        </div>
-        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
-            <a href="{{route('db.konsumen')}}" class="text-decoration-none">
-                <img src="{{asset('images/customer.svg')}}" alt="" width="70">
-                <h5 class="mt-2">BIODATA<br>KONSUMEN</h5>
-            </a>
-        </div>
-        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
-            <a href="{{route('db.stok-ppn')}}" class="text-decoration-none">
-                <img src="{{asset('images/stok-ppn.svg')}}" alt="" width="70">
-                <h5 class="mt-2">STOK & HARGA<br>BARANG PPN</h5>
-            </a>
-        </div>
-        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
-            <a href="{{route('db.stok-non-ppn')}}" class="text-decoration-none">
-                <img src="{{asset('images/stok-non-ppn.svg')}}" alt="" width="70">
-                <h5 class="mt-2">STOK & HARGA<br>BARANG NON PPN</h5>
-            </a>
-        </div>
-
-        @endif
-    </div>
-    <div class="row justify-content-left">
-        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
             <a href="{{route('db.cost-operational')}}" class="text-decoration-none">
                 <img src="{{asset('images/cost-operational.svg')}}" alt="" width="70">
                 <h5 class="mt-2">KATEGORI<br>COST OPERASIONAL</h5>
@@ -106,13 +72,48 @@
             </a>
         </div>
 
+
+    </div>
+    <div class="row justify-content-left">
+        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
+            <a href="{{route('db.staff')}}" class="text-decoration-none">
+                <img src="{{asset('images/karyawan.svg')}}" alt="" width="70">
+                <h5 class="mt-2">BIODATA<br>DIREKSI & STAFF</h5>
+            </a>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
+            <a href="{{route('db.supplier')}}" class="text-decoration-none">
+                <img src="{{asset('images/supplier.svg')}}" alt="" width="70">
+                <h5 class="mt-2">BIODATA<br>SUPPLIER<br></h5>
+            </a>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
+            <a href="{{route('db.konsumen')}}" class="text-decoration-none">
+                <img src="{{asset('images/customer.svg')}}" alt="" width="70">
+                <h5 class="mt-2">BIODATA<br>KONSUMEN</h5>
+            </a>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
+            <a href="{{route('db.stok-ppn')}}" class="text-decoration-none">
+                <img src="{{asset('images/stok-ppn.svg')}}" alt="" width="70">
+                <h5 class="mt-2">STOK & HARGA<br>BARANG PPN</h5>
+            </a>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
+            <a href="{{route('db.stok-non-ppn')}}" class="text-decoration-none">
+                <img src="{{asset('images/stok-non-ppn.svg')}}" alt="" width="70">
+                <h5 class="mt-2">STOK & HARGA<br>BARANG NON PPN</h5>
+            </a>
+        </div>
         <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
             <a href="{{route('home')}}" class="text-decoration-none">
                 <img src="{{asset('images/dashboard.svg')}}" alt="" width="70">
                 <h5 class="mt-2">DASHBOARD</h5>
             </a>
         </div>
+        @endif
     </div>
+
 
 
 </div>
