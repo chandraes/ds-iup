d0656fd6eb08ef4a88bd0d7741094d23b3076698
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-07-15 15:16:23 +0700

    Update column labels in barang index view

--- resources/views/db/index.blade.php
+++ resources/views/db/index.blade.php
@@ -67,7 +67,7 @@
         <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
             <a href="{{route('db.cost-operational')}}" class="text-decoration-none">
                 <img src="{{asset('images/cost-operational.svg')}}" alt="" width="70">
-                <h5 class="mt-2">KATEGORI COST<br>OPERASIONAL</h5>
+                <h5 class="mt-2">KATEGORI<br>COST OPERASIONAL</h5>
             </a>
         </div>
         <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
@@ -79,7 +79,7 @@
         <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
             <a href="{{route('db.unit')}}" class="text-decoration-none">
                 <img src="{{asset('images/unit.svg')}}" alt="" width="70">
-                <h5 class="mt-2">KATEGORI MERK &<br>TIPE UNIT</h5>
+                <h5 class="mt-2">KATEGORI<br>MERK & TIPE<br>UNIT</h5>
             </a>
         </div>
         <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
