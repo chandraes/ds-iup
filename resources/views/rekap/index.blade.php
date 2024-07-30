8684fbbf78bf1c54489f26142a4e2bcbf5aabb9d
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-07-30 15:34:03 +0700

    refactor: Update 'rekap/index.blade.php' to use more descriptive text for invoice pembelian

--- resources/views/rekap/index.blade.php
+++ resources/views/rekap/index.blade.php
@@ -40,7 +40,7 @@
         <div class="col-lg-3 mt-3 mb-3 text-center">
             <a href="#" class="text-decoration-none">
                 <img src="{{asset('images/rekap-invoice.svg')}}" alt="" width="70">
-                <h3 class="mt-2">INVOICE BELI</h3>
+                <h3 class="mt-2">INVOICE PEMBELIAN</h3>
             </a>
         </div>
         <div class="col-lg-3 mt-3 mb-3 text-center">
