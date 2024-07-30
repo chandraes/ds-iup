8d5384890c1ce6881df337b5e53b99f3667380c3
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-07-30 16:58:17 +0700

    refactor: Update 'KasBesar.php' to improve display of saldo and modal investor
    This commit modifies the 'KasBesar.php' file to improve the display of saldo and modal investor. It adds conditional statements to determine whether to display PPN or NON PPN values for saldo and modal investor. This change enhances the readability and clarity of the displayed information.

--- app/Models/KasBesar.php
+++ app/Models/KasBesar.php
@@ -132,6 +132,18 @@ class KasBesar extends Model
                 'modal_investor' => $this->modalInvestorTerakhir(0),
             ];
 
+            if ($data['ppn_kas'] == 1) {
+               $addPesan = "Sisa Saldo Kas Besar PPN: \n".
+                           "Rp. ".number_format($kasPpn['saldo'], 0, ',', '.')."\n\n".
+                           "Total Modal Investor PPN: \n".
+                           "Rp. ".number_format($kasPpn['modal_investor'], 0, ',', '.')."\n\n";
+            } else {
+                $addPesan = "Sisa Saldo Kas Besar NON PPN: \n".
+                            "Rp. ".number_format($kasNonPpn['saldo'], 0, ',', '.')."\n\n".
+                            "Total Modal Investor NON PPN: \n".
+                            "Rp. ".number_format($kasNonPpn['modal_investor'], 0, ',', '.')."\n\n";
+            }
+
             // sum modal investor
             $totalModal = $kasPpn['modal_investor'] + $kasNonPpn['modal_investor'];
 
@@ -149,11 +161,8 @@ class KasBesar extends Model
                         "Nama    : ".$store->nama_rek."\n".
                         "No. Rek : ".$store->no_rek."\n\n".
                         "==========================\n".
-                        "Sisa Saldo Kas Besar PPN: \n".
-                        "Rp. ".number_format($kasPpn['saldo'], 0, ',', '.')."\n\n".
-                        "Sisa Saldo Kas Besar  NON PPN: \n".
-                        "Rp. ".number_format($kasNonPpn['saldo'], 0, ',', '.')."\n\n".
-                        "Total Modal Investor : \n".
+                        $addPesan.
+                        "Grand Total Modal Investor : \n".
                         "Rp. ".number_format($totalModal, 0, ',', '.')."\n\n".
                         "Terima kasih 🙏🙏🙏\n";
 
