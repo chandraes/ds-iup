26f86a4338fd36babc5d3db9833780a386bdb335
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-08-15 17:38:47 +0700

    UP

--- resources/views/billing/index.blade.php
+++ resources/views/billing/index.blade.php
@@ -116,6 +116,12 @@
             </a>
         </div>
         <div class="col-lg-3 col-md-3 col-sm-6 text-center mt-5">
+            <a href="#" class="text-decoration-none">
+                <img src="{{asset('images/form-cost-operational.svg')}}" alt="" width="70">
+                <h4 class="mt-2">GANTI RUGI<br>BARANG HILANG</h4>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 text-center mt-5">
             <a href="{{route('home')}}" class="text-decoration-none">
                 <img src="{{asset('images/dashboard.svg')}}" alt="" width="70">
                 <h4 class="mt-2">DASHBOARD
