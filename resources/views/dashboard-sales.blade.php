57b510a644f411f4ddfa15fbfad573e515479a92
UPT TIK Unsri <ka.tik@unsri.ac.id>
2025-05-17 14:59:32 +0700

    fix: Update sales order labels for clarity in dashboard view

--- resources/views/dashboard-sales.blade.php
+++ resources/views/dashboard-sales.blade.php
@@ -14,13 +14,13 @@
      <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
         <a href="{{route('sales.order', ['kas_ppn' => 1])}}" class="text-decoration-none">
             <img src="{{asset('images/order-ppn.svg')}}" alt="" width="70">
-            <h5 class="mt-2">SALES ORDER<br>PPN</h5>
+            <h5 class="mt-2">SALES ORDER<br>BARANG A</h5>
         </a>
     </div>
     <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
         <a href="{{route('sales.order', ['kas_ppn' => 0])}}" class="text-decoration-none">
             <img src="{{asset('images/order-non-ppn.svg')}}" alt="" width="70">
-            <h5 class="mt-2">SALES ORDER<br>NON PPN</h5>
+            <h5 class="mt-2">SALES ORDER<br>BARANG B</h5>
         </a>
     </div>
      <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
