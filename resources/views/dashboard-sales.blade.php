fd619083de29bd3b2ce51b01d06801432b260ad9
UPT TIK Unsri <ka.tik@unsri.ac.id>
2025-05-17 13:34:49 +0700

    fix: Adjust layout and headings for better organization in dashboard sales view

--- resources/views/dashboard-sales.blade.php
+++ resources/views/dashboard-sales.blade.php
@@ -1,11 +1,17 @@
+
 <div class="row justify-content-left">
+    <h3>TRANSAKSI</h3>
     <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
         <a href="{{route('sales.stok')}}" class="text-decoration-none">
             <img src="{{asset('images/stok-ppn.svg')}}" alt="" width="70">
             <h5 class="mt-2">STOK &<br>HARGA JUAL<br>BARANG</h5>
         </a>
     </div>
-    <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
+</div>
+<hr>
+<div class="row justify-content-left">
+    <h3>REKAP</h3>
+     <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
         <a href="{{route('sales.order', ['kas_ppn' => 1])}}" class="text-decoration-none">
             <img src="{{asset('images/order-ppn.svg')}}" alt="" width="70">
             <h5 class="mt-2">SALES ORDER<br>PPN</h5>
@@ -23,9 +29,10 @@
             <h5 class="mt-2">PRE ORDER</h5>
         </a>
     </div>
-
 </div>
+<hr>
 <div class="row justify-content-left">
+    <h3>STATISTIK</h3>
    <div class="col-lg-2 col-md-2 col-sm-4 my-4 text-center">
         <a href="{{route('sales.omset-harian')}}" class="text-decoration-none">
             <img src="{{asset('images/omset-sales.svg')}}" alt="" width="70">
